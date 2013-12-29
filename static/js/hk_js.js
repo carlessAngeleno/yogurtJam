71e11e14956084b8695d37b7e50895d34549f7c3
carlessAngeleno <daydream.han@gmail.com>
2013-12-28 22:45:17 -0800

    Auto-populate search date range

--- static/js/hk_js.js
+++ static/js/hk_js.js
@@ -583,4 +583,23 @@ $(function() {
         // }
     });
 
+    // auto-populate date range
+    var today = new Date();
+    var dd = today.getDate();
+    var dd_min = 1;
+
+    var mm = today.getMonth()+1; //January is 0!
+
+    var yyyy = today.getFullYear();
+    
+    if (dd < 10) {dd = '0' + dd} 
+    
+    if (mm < 10) {mm = '0' + mm} 
+    
+    today = mm + '/' + dd + '/' + yyyy;
+    var min_day = mm + '/' + dd_min + '/' + yyyy;
+
+    $("#memoryDateMin").val(min_day);
+    $("#memoryDateMax").val(today);
+
 });
