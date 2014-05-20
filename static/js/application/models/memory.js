a2bcb525854ea1e53d7dc084f4ae651f07106ce8
carlessAngeleno <daydream.han@gmail.com>
2014-05-19 22:01:08 -0700

    Add mockup for map nav controllers

--- static/js/application/models/memory.js
+++ static/js/application/models/memory.js
@@ -15,10 +15,10 @@ Memories.Memory = DS.Model.extend({
 
 // FIXTURE
 Memories.Memory.FIXTURES = [
-  {"story": "1", "tag1": "sdfs", "title": "firework", "video_id": "QGJuMBdaqIw", "g_place": "Hotel Cir S", "id": 1, "lat": 32.7589995, "lng": -117.1763604, "artist": "katy perry", "time_added": "2013-05-17 05:42:30", "memoryDateShare": "2014-05-01 00:00:00"}, 
-  {"story": "4", "tag1": "sdfs", "title": "firework", "video_id": "QGJuMBdaqIw", "g_place": "Hotel Cir S", "id": 4, "lat": 32.7589995, "lng": -117.1763604, "artist": "katy perry", "time_added": "2013-05-17 05:42:30", "memoryDateShare": "2014-05-01 00:00:00"},
-  {"story": "5", "tag1": "sdfs", "title": "firework", "video_id": "QGJuMBdaqIw", "g_place": "Hotel Cir S", "id": 5, "lat": 32.7589995, "lng": -117.1763604, "artist": "katy perry", "time_added": "2013-05-17 05:42:30", "memoryDateShare": "2014-05-01 00:00:00"}, 
-  {"story": "2", "tag1": "sdfs", "title": "firework", "video_id": "QGJuMBdaqIw", "g_place": "Hotel Cir S", "id": 2, "lat": 32.7589995, "lng": -117.1763604, "artist": "katy perry", "time_added": "2013-05-17 05:42:30", "memoryDateShare": "2014-05-01 00:00:00"}, 
-  {"story": "3", "tag1": "sdfs", "title": "firework", "video_id": "QGJuMBdaqIw", "g_place": "Hotel Cir S", "id": 3, "lat": 32.7589995, "lng": -117.1763604, "artist": "katy perry", "time_added": "2013-05-17 05:42:30", "memoryDateShare": "2014-05-01 00:00:00"},   
-  {"story": "6", "tag1": "sdfs", "title": "firework", "video_id": "QGJuMBdaqIw", "g_place": "Hotel Cir S", "id": 6, "lat": 32.7589995, "lng": -117.1763604, "artist": "katy perry", "time_added": "2013-05-17 05:42:30", "memoryDateShare": "2014-05-01 00:00:00"}  
+  // {"story": "1", "tag1": "sdfs", "title": "firework", "video_id": "QGJuMBdaqIw", "g_place": "Hotel Cir S", "id": 1, "lat": 32.7589995, "lng": -117.1763604, "artist": "katy perry", "time_added": "2013-05-17 05:42:30", "memoryDateShare": "2014-05-01 00:00:00"}, 
+  // {"story": "4", "tag1": "sdfs", "title": "firework", "video_id": "QGJuMBdaqIw", "g_place": "Hotel Cir S", "id": 4, "lat": 32.7589995, "lng": -117.1763604, "artist": "katy perry", "time_added": "2013-05-17 05:42:30", "memoryDateShare": "2014-05-01 00:00:00"},
+  // {"story": "5", "tag1": "sdfs", "title": "firework", "video_id": "QGJuMBdaqIw", "g_place": "Hotel Cir S", "id": 5, "lat": 32.7589995, "lng": -117.1763604, "artist": "katy perry", "time_added": "2013-05-17 05:42:30", "memoryDateShare": "2014-05-01 00:00:00"}, 
+  // {"story": "2", "tag1": "sdfs", "title": "firework", "video_id": "QGJuMBdaqIw", "g_place": "Hotel Cir S", "id": 2, "lat": 32.7589995, "lng": -117.1763604, "artist": "katy perry", "time_added": "2013-05-17 05:42:30", "memoryDateShare": "2014-05-01 00:00:00"}, 
+  // {"story": "3", "tag1": "sdfs", "title": "firework", "video_id": "QGJuMBdaqIw", "g_place": "Hotel Cir S", "id": 3, "lat": 32.7589995, "lng": -117.1763604, "artist": "katy perry", "time_added": "2013-05-17 05:42:30", "memoryDateShare": "2014-05-01 00:00:00"},   
+  // {"story": "6", "tag1": "sdfs", "title": "firework", "video_id": "QGJuMBdaqIw", "g_place": "Hotel Cir S", "id": 6, "lat": 32.7589995, "lng": -117.1763604, "artist": "katy perry", "time_added": "2013-05-17 05:42:30", "memoryDateShare": "2014-05-01 00:00:00"}  
 ];
