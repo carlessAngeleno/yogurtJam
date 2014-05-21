59c7e791bbe87dbce4d64258475b064713e0848b
carlessAngeleno <daydream.han@gmail.com>
2014-05-20 22:17:23 -0700

    Remove unnecessary JS code referencing to #select_place_button explicitly

--- static/js/application/views/g_places_input_view.js
+++ static/js/application/views/g_places_input_view.js
@@ -1,58 +1,49 @@
 Memories.GPlacesInputView = Ember.TextField.extend({
   didInsertElement: function() {
     var acOptions = {
-        types: ['establishment', 'geocode']
+      types: ['establishment', 'geocode']
     };    
     autocomplete = new google.maps.places.Autocomplete(
-        this.$()[0],
-        acOptions
+      this.$()[0],
+      acOptions
     );   
     autocomplete.bindTo('bounds', map);
     google.maps.event.addListener(autocomplete, 'place_changed', function() {
         
-        var infoWindow = new google.maps.InfoWindow();
+      var infoWindow = new google.maps.InfoWindow();
         
-        // plant new marker  
-        var marker = new google.maps.Marker({
-            map: map
-        });
-        
-        // extract place info
-        var place = autocomplete.getPlace();
-        
-        // map focuses onto place         
-        if (place.geometry.viewport) {
-            map.fitBounds(place.geometry.viewport);
-        } 
-        else {
-            map.setCenter(place.geometry.location);
-            map.setZoom(17);
-        }
+      // plant new marker  
+      var marker = new google.maps.Marker({
+        map: map
+      });
+      
+      // extract place info
+      var place = autocomplete.getPlace();
+      
+      // map focuses onto place         
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      } 
+      else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);
+      }
 
-        // take down place's location (lat/long)
-        var new_place_lat_lng = place.geometry.location;
-    
-        // put a marker on the location 
-        marker.setPosition(new_place_lat_lng);
-        
-        // infowindow to pop up will have the name of the place
-        infoWindow.setContent('<div><strong>' + place.name + '</strong><br>');
-       
+      // take down place's location (lat/long)
+      var new_place_lat_lng = place.geometry.location;
+  
+      // put a marker on the location 
+      marker.setPosition(new_place_lat_lng);
+      
+      // infowindow to pop up will have the name of the place
+      infoWindow.setContent('<div><strong>' + place.name + '</strong><br>');
+     
+      infoWindow.open(map, marker);
+
+      google.maps.event.addListener(marker,'click',function(e){
         infoWindow.open(map, marker);
-
-        google.maps.event.addListener(marker,'click',function(e){
-            infoWindow.open(map, marker);
-        });
-        
-        // we save into lat and lng vars if user confirms this is the right place         
-        $("#select_place_button").click(function() {         
-            new_place_lat = new_place_lat_lng.lat(); 
-            new_place_lng = new_place_lat_lng.lng();
-            new_place_g_place = place.name;            
-        });        
-    }); 
-
-
+      });    
+    });
     this.$().focus();
   }
 });
