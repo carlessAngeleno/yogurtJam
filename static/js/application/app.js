9bc090ad1ecdd59db2cebc1e38ef83ddec90755a
carlessAngeleno <daydream.han@gmail.com>
2014-05-20 20:03:22 -0700

    Delete dead code

--- static/js/application/app.js
+++ static/js/application/app.js
@@ -106,59 +106,7 @@ function initialize() {
         document.getElementById('autocomplete'),acOptions
     );
     
-    autocomplete.bindTo('bounds', map);
-
-    //once user types into Google Places text box and selects a location, pull relevant info
-       
-    // // infowindow object (i.e. pop-up window in Google Maps)
-    // var infowindow = new google.maps.InfoWindow({
-    //     maxWidth: 240
-    // });   
-    
-    // // once place "changes" (user selects location using the autocomplete text box)   
-    // google.maps.event.addListener(autocomplete, 'place_changed', function() {
-        
-    //     var infoWindow = new google.maps.InfoWindow();
-        
-    //     // plant new marker  
-    //     var marker = new google.maps.Marker({
-    //         map: map
-    //     });
-        
-    //     // extract place info
-    //     var place = autocomplete.getPlace();
-        
-    //     // map focuses onto place         
-    //     if (place.geometry.viewport) {
-    //         map.fitBounds(place.geometry.viewport);
-    //     } 
-    //     else {
-    //         map.setCenter(place.geometry.location);
-    //         map.setZoom(17);
-    //     }
-
-    //     // take down place's location (lat/long)
-    //     var new_place_lat_lng = place.geometry.location;
-    
-    //     // put a marker on the location 
-    //     marker.setPosition(new_place_lat_lng);
-        
-    //     // infowindow to pop up will have the name of the place
-    //     infoWindow.setContent('<div><strong>' + place.name + '</strong><br>');
-       
-    //     infoWindow.open(map, marker);
-
-    //     google.maps.event.addListener(marker,'click',function(e){
-    //         infoWindow.open(map, marker);
-    //     });
-        
-    //     // we save into lat and lng vars if user confirms this is the right place         
-    //     $("#select_place_button").click(function() {         
-    //         new_place_lat = new_place_lat_lng.lat(); 
-    //         new_place_lng = new_place_lat_lng.lng();
-    //         new_place_g_place = place.name;            
-    //     });        
-    // });      
+    autocomplete.bindTo('bounds', map);    
 };
   
 function drawOnMap(markers) {
