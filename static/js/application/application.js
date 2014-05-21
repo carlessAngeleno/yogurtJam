4678160a9381b488cfe14dde8f8a9cf2320e4c53
carlessAngeleno <daydream.han@gmail.com>
2014-05-20 22:02:22 -0700

    Move initial page onload code to Memories app's ready call - retire legacy javascript file app.js officially

--- static/js/application/application.js
+++ static/js/application/application.js
@@ -1,6 +1,24 @@
 window.Memories = Ember.Application.create({
-	// rootElement: "#main_body",
-    LOG_TRANSITIONS: true
+  // rootElement: "#main_body",
+    LOG_TRANSITIONS: true,
+    ready: function() {
+      $("#player").hide();
+
+      // create YT player
+      var player;
+      var tag = document.createElement('script');
+      tag.src = "//www.youtube.com/iframe_api"
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+      
+      // Need this function to talk to the API
+      function onYouTubeIframeAPIReady() {
+          player = new YT.Player('player', {
+              height: '260',
+              width: '360'
+          });
+      }         
+    }
 });
 
 // Memories.ApplicationAdapter = DS.FixtureAdapter.extend();
