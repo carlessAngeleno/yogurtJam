ed2a8f25e13165303f8e1cb8c8bb1be5855cdf41
carlessAngeleno <daydream.han@gmail.com>
2019-01-20 19:34:36 -0800

    Delete all

--- static/js/application/helpers/helpers.js
+++
@@ -1,7 +0,0 @@
-Ember.Handlebars.helper('formattedDate', function(raw) {
-  return moment(raw).format('LL');
-});
-
-Ember.Handlebars.helper('timeAgo', function(date){
-    return moment(date).fromNow();
-});
